cdc94d68189cc04bce956b8b83e5a6eeea181821
shaheerairaj <shaheerairajahmed@gmail.com>
2025-03-21 01:39:29 +0400

    created utils file and started on basic agent logic

--- submissions-team/shaheer-airaj/shaheer-agentic-routine.py
+++ submissions-team/shaheer-airaj/shaheer-agentic-routine.py
@@ -1,10 +1,11 @@
 import os
 import random
+import json
 from openai import OpenAI
 from pydantic import BaseModel
 from dotenv import load_dotenv
-from typing import List, Dict, Any, Optional
-from utils import helper_functions, execute_tool_call
+from typing import List, Dict, Any, Optional, Type
+from utils import function_to_schema, execute_tool_call
 
 load_dotenv()
 
@@ -15,14 +16,25 @@ if not OPENAI_API_KEY:
 client = OpenAI()
 
 
+################ Classes ################
+
 class Agent(BaseModel):
     name: str = "Agent"
     model: str = "gpt-4o-mini"
     instructions: str = "You are a helpful agent"
-    tools: list = None
+    tools: Optional[list] = None
+    response_format: Optional[Type[BaseModel]] = None
 
-def call_exercise_agent():
-    return True
+class User(BaseModel):
+    language: str = "English"
+    experience_level: str = "beginner"
+    learning_goal: str = "learn the basics"
+
+
+
+############ Tools ################
+def call_information_extraction_agent():
+    return information_extraction_agent
 
 def phrase_generator(language: str = "french"):
     phrases = {
@@ -47,6 +59,42 @@ def phrase_generator(language: str = "french"):
         
     return random.choice(phrases[language.lower()])
 
+
+def execute_tool_call(tool_call, tools, agent_name):
+    name = tool_call.function.name
+    args = json.loads(tool_call.function.arguments)
+
+    print(f"{agent_name}:", f"{name}({args})")
+
+    return tools[name](**args)
+
+
+def chat_completions(agent, messages):
+    tool_schemas = [function_to_schema(tool) for tool in agent.tools]
+    tool_map = {tool.__name__: tool for tool in agent.tools}
+
+    response = client.chat.completions.create(
+        model = agent.model,
+        messages = [{"role": "system", "content": agent.instructions}] + messages,
+        tools = tool_schemas
+    )
+
+    return response.choices[0].message, tool_schemas, tool_map
+
+def chat_parse(agent, messages):
+    # tool_schemas = [function_to_schema(tool) for tool in agent.tools]
+    # tool_map = {tool.__name__: tool for tool in agent.tools}
+
+    response = client.beta.chat.completions.parse(
+        model = agent.model,
+        messages = [{"role": "system", "content": agent.instructions}] + messages,
+        # tools = tool_schemas,
+        response_format = agent.response_format
+    )
+
+    return response.choices[0].message
+################ Agents ################
+
 conversation_agent = Agent(
     name = "Conversation Agent",
     instructions = """
@@ -60,9 +108,21 @@ conversation_agent = Agent(
         The point is to gain a deep understanding of the user's language learning goals.
 
         Once you have a deep understanding of the user's language learning goals, you are to
-        pass the user's information to the Exercise Agent.
+        pass the user's information to the Information Extraction Agent.
     """,
-    tools = [call_exercise_agent]
+    tools = [call_information_extraction_agent]
+)
+
+information_extraction_agent = Agent(
+    name = "Information Extraction Agent",
+    instructions = """
+        You are a helpful assistant that can extract information about the user's language learning goals
+        from a conversation with the user.
+
+        You will be given a conversation between a user and a conversation agent.
+        Your job is to extract the information about the user's language learning goals from the conversation.
+    """,
+    response_format = User
 )
 
 exercise_agent = Agent(
@@ -81,31 +141,28 @@ exercise_agent = Agent(
 
         If the user answers the phrase correctly, you can then ask them to generate a response to a different phrase.
         If they do not answer correctly, you can ask them to try again and maybe give them a hint.
-    """,
-    tools = [phrase_generator]
+    """
 )
 
 
+#########################################
+
+
+
 def run_turn(agent, messages):
-    print("Assitant: Hello, how can I help you today?")
-    user_input = ""
+    current_agent = agent
+
+    print("Assitant: Hello, I am your language learning assistant. Can you tell me what language you want to learn?")
+    user_input = input("\nYou: ")
 
     while user_input != "exit":
-
-        tool_schemas = [helper_functions.function_to_schema(tool) for tool in agent.tools]
-        tool_map = {tool.__name__: tool for tool in agent.tools}
         
-        user_input = input("\nYou: ")
         messages.append({"role": "user", "content": user_input})
 
-        # Get response from the model
-        response = client.chat.completions.create(
-        model = agent.model,
-        messages = [{"role": "system", "content": agent.instructions}] + messages,
-        tools = tool_schemas,
-        )
+        print("Current agent: ", current_agent.name)
 
-        message = response.choices[0].message
+        # Get response from the model
+        message, tool_schemas, tool_map = chat_completions(current_agent, messages)
         messages.append(message)
 
         if message.content:
@@ -115,8 +172,14 @@ def run_turn(agent, messages):
             print("\nMessage: ", message)
             print("\nTool calls: ", message.tool_calls)
             for tool_call in message.tool_calls:
-                result = execute_tool_call(tool_call, tool_map, agent.name)
+                result = execute_tool_call(tool_call, tool_map, current_agent.name)
                 print("\nTool call result: ", result)
+                print("\nTool result is Agent? ", type(result) is Agent)
+
+                if type(result) is Agent:
+                    current_agent = result
+
+                    print("\nConversation has been passed on to: ", current_agent.name)
 
             result_message = {
                 "role":"tool",
@@ -124,16 +187,15 @@ def run_turn(agent, messages):
                 "content": str(result)
             }
             messages.append(result_message)
+
+            message = chat_parse(current_agent, messages)
+            messages.append(message)
+            print("Message from agent: ", message)
+
+        
+        user_input = input("\nYou: ")
             
     return messages
-
-def execute_tool_call(tool_call, tools, agent_name):
-    name = tool_call.function.name
-    args = json.loads(tool_call.function.arguments)
-
-    print(f"{agent_name}:", f"{name}({args})")
-
-    return tools[name](**args)
 
 
 run_turn(conversation_agent, [])
